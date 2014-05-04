e143c9b06825dc11a016f4ee668df97a74fa4b13
stevelowenthal <steve@datastax.com>
2014-05-03 17:59:16 -0700

    Add license

--- src/main/java/org/apache/cassandra/jmeter/config/CassandraSessionFactory.java
+++ src/main/java/org/apache/cassandra/jmeter/config/CassandraSessionFactory.java
@@ -1,5 +1,21 @@
 package org.apache.cassandra.jmeter.config;
 
+/*
+ * Copyright 2014 Steven Lowenthal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.QueryOptions;
 import com.datastax.driver.core.Session;
@@ -10,11 +26,6 @@ import com.datastax.driver.core.policies.LoadBalancingPolicy;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * DataStax Academy Sample Application
- * <p/>
- * Copyright 2013 DataStax
- */
 public class CassandraSessionFactory {
 
   // This class supports both multiple cluster objects for different clusters, as well as
